39d3f8669b40cefc28e3ade7936eafc806fe3756
Nnamdi Okeke <nnamdi@Nnamdis-MBP.lan>
2024-05-23 13:35:02 -0500

    modified backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key     = "dev/terraformstatefile"
-    bucket = "bootcamp34-nnamdi"
-    region = "us-west-2"
+    key            = "dev/terraformstatefile"
+    bucket         = "bootcamp34-nnamdi"
+    region         = "us-west-2"
     dynamodb_table = "for_terraform_locking"
   }
 }
