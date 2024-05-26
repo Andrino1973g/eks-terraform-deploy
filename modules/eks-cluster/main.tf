09818cdfd2dc3f4b7367654b9c28d055bb7055f7
Nnamdi Okeke <nokeke67@gmail.com>
2024-05-26 06:19:44 -0500

    Update main.tf

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -58,6 +58,20 @@ module "eks" {
     }
   }
 
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
+}
+
   # aws-auth configmap
   manage_aws_auth_configmap = true
   #create_aws_auth_configmap = true
